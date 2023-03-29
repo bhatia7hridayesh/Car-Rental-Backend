ec64ad27ac6aa17317d3c7302aa7a931dc73dafb
Hridayesh Bhatia <hridayeshbhatia2@gmail.com>
2023-03-29 15:34:33 +0530

    Testing

--- backend/settings.py
+++ backend/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-pln=dig!+pp8f5c_)b9fubypforhlfn9yghbye*9$+pqtd*jnc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -138,6 +138,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
