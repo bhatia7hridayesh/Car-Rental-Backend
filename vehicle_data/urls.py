5b00db14bbbec834523f818923f329e5ba82e1e7
Hridayesh Bhatia <hridayeshbhatia2@gmail.com>
2023-03-30 19:46:55 +0530

    f

--- vehicle_data/urls.py
+++ vehicle_data/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path("create/", Create_Vehicle.as_view()),
     path("agency-vehicles/", My_Vehicles.as_view()),
     path("booking-history/", Bookings_history.as_view()),
-    path("update-vehicle/<pk:int>", Update_Vehicle.as_view()),
+    path("update-vehicle/<int:pk>", Update_Vehicle.as_view()),
     #User URLS
     path("rent-vehicle/", Rent_Vehicle.as_view()),
     path("user-bookings/", My_Bookings.as_view()),
