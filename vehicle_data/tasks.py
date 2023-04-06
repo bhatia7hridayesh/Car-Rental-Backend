c91e3fa1f28249574d1ee158f3e189b9fbb90ce3
Hridayesh Bhatia <hridayeshbhatia2@gmail.com>
2023-04-06 10:15:21 +0530

    commit

--- vehicle_data/tasks.py
+++ vehicle_data/tasks.py
@@ -2,7 +2,7 @@ from .models import Vehicle_Bookings_Log
 from django.utils import timezone
 def delete_expired_discounts():
     current_time = timezone.now()
-    end_of_rent = Vehicle_Bookings_Log.objects.filter(returned_on = current_time)
+    end_of_rent = Vehicle_Bookings_Log.objects.filter(returned_on__lte = current_time)
     for log in end_of_rent:
         vehicle = log.vehicle
         vehicle.is_booked = False
